1cc9317a34c1fe344ac97047df2c94b201c62ab8
abhishekishan <abhishekishan@live.com>
2018-03-22 10:55:14 +0530

    Some Changes

--- src/com/caps/jdbc/Test1.java
+++ src/com/caps/jdbc/Test1.java
@@ -3,6 +3,7 @@ import java.io.FileReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
 import com.mysql.jdbc.Driver;
@@ -23,8 +24,8 @@ public class Test1
 			java.sql.Driver driverRef = new Driver();
 			DriverManager.registerDriver(driverRef);
 
-			/*String dbURL = "jdbc:mysql://localhost:3306/caps50_db?user=root&password=74123";
-			con = DriverManager.getConnection(dbURL);*/
+			String dbURL = "jdbc:mysql://localhost:3306/caps50_db";
+			con = DriverManager.getConnection(dbURL,prop);
 
 			String query="SELECT * FROM person_info";
 			stmt=con.createStatement();
@@ -38,14 +39,26 @@ public class Test1
 				System.out.println(regno+" "+fname+" "+lname);
 			}
 		}
-		catch(Exception e)
+		catch(SQLException e)
 		{
 			e.printStackTrace();
 		}
 		finally
 		{
-			con.close();
-			stmt.close();
-		}	
+			try{
+				if(stmt != null)
+				{
+					stmt.close();
+				}
+				if(con != null)
+				{
+					con.close();
+				}
+			}
+			catch(Exception e)
+			{
+				e.printStackTrace();
+			}
+		}
 	}
 }
